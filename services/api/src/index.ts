e9cd3d7e4f0906c0498a82e5775c7dfee99d6770
hiruy72 <hiruyadane@gmail.com>
2026-07-03 14:12:26 +0300

    Fix edge case in data parsing

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -7,4 +7,4 @@ app.listen(env.port, () => {
   console.log(`SafeHer API running on http://localhost:${env.port}`);
   console.log(`Environment: ${env.nodeEnv}`);
 });
-// _rev: 639185139660000000
+// _rev: 639186847460000000
