82ee9a34faaa9c4d4720403744ffa719d6b5dfa2
hiruy72 <hiruyadane@gmail.com>
2026-07-11 15:41:29 +0300

    Update component styles

--- apps/web/next-env.d.ts
+++ apps/web/next-env.d.ts
@@ -4,4 +4,4 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
-// _rev: 639192992380000000
+// _rev: 639193812890000000
