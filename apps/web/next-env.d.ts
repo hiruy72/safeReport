131127a8b21d4464911d90334505ec579d09b6ef
hiruy72 <hiruyadane@gmail.com>
2026-07-21 21:32:04 +0300

    Enhance mobile UI consistency

--- apps/web/next-env.d.ts
+++ apps/web/next-env.d.ts
@@ -4,4 +4,4 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
-// _rev: 639196482200000000
+// _rev: 639202663240000000
